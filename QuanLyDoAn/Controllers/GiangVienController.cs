87f75c89e0dffb7ae492d060981155e3c92f345c
Nhattruong1998 <Nhattruong1998@gmail.com>
2019-11-21 19:44:00 +0700

    Add layout GiangVien

---
+++ QuanLyDoAn/Controllers/GiangVienController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyDoAn.Controllers
+{
+    public class GiangVienController : Controller
+    {
+        // GET: GiangVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult QuanLySinhVien()
+        {
+            return View();
+        }
+        public ActionResult ThongTinDoAn()
+        {
+            return View();
+        }
+        public ActionResult NhomDoAn()
+        {
+            return View();
+        }
+        public ActionResult ChamDiemDoAn()
+        {
+            return View();
+        }
+        public ActionResult PhanChiaCongViec()
+        {
+            return View();
+        }
+    }
+}
