7fa17d8df851b05170c0a6c1440058f8567645e0
ManhHungPham <48708928+ManhHungPham@users.noreply.github.com>
2019-12-04 15:20:46 +0700

    newnew

---
+++ qldoansvhutech/qldoansvhutech/Controllers/GiangVienController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qldoansvhutech.Controllers
+{
+    public class GiangVienController : Controller
+    {
+        // GET: GiangVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult QuanLySinhVien()
+        {
+            return View();
+        }
+        public ActionResult ThongTinDoAn()
+        {
+            return View();
+        }
+        public ActionResult NhomDoAn()
+        {
+            return View();
+        }
+        public ActionResult ChamDiemDoAn()
+        {
+            return View();
+        }
+        public ActionResult PhanChiaCongViec()
+        {
+            return View();
+        }
+    }
+}
---
+++ qldoansvhutech/qldoansvhutech/Controllers/SinhVienController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qldoansvhutech.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        // GET: SinhVien
+        public ActionResult Thongtinsinhvien()
+        {
+            return View();
+        }
+        public ActionResult Dangkydoan()
+        {
+            return View();
+        }
+        public ActionResult Dangkynhom()
+        {
+            return View();
+        }
+        public ActionResult Nhom()
+        {
+            return View();
+        }
+    }
+}
