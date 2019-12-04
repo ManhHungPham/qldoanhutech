7a8d95bc07a247eb2352893e71d8d117bab9d3e8
ManhHungPham <48708928+ManhHungPham@users.noreply.github.com>
2019-12-04 15:12:05 +0700

    new

---
+++ qldoanhutech-master/qldoansv/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace qldoansv.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Dangnhap()
+        {
+            return View();
+        }
+    }
+}
