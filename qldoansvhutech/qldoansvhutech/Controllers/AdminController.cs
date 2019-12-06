09d97acc60d17e5b492fa9444be4eeb713d966c6
Nhattruong1998 <Nhattruong1998@gmail.com>
2019-12-06 10:59:32 +0700

    day list sv

---
+++ qldoansvhutech/qldoansvhutech/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qldoansvhutech.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ThongTin()
+        {
+            return View();
+        }
+        public ActionResult DanhSachGiangVien()
+        {
+            return View();
+        }
+        public ActionResult PhanChiaHuongDan()
+        {
+            return View();
+        }
+        public ActionResult DanhSachSinhVien()
+        {
+            return View();
+        }
+        public ActionResult QuanLyDoAn()
+        {
+            return View();
+        }
+    }
+}
